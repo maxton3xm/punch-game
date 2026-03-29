c89c6765e5f53986f50c3013b0ee43c8767ac863
jellou_ <liamprogames14@gmail.com>
2026-03-29 13:50:15 -0400

    fix

--- punch-game.py
+++ punch-game.py
@@ -2,10 +2,28 @@ import turtle
 
 t = turtle.Turtle()
 t.color("blue")
+t.speed(0)
 
-def on_key_press(event):
-  if event.char == "a":
-    t.color("red")
+screen = turtle.Screen()
+screen.tracer(0)
+screen.listen()
 
-turtle.done()
-    
+def move_forward():
+    t.forward(20)
+
+def move_backward():
+    t.backward(20)
+
+def turn_left():
+    t.left(15)
+
+def turn_right():
+    t.right(15)
+running = True
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+
+while running:
+  screen.update()
